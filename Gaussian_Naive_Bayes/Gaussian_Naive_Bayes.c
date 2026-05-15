29384ea709dce8ad623d55e1886247c48ea0227c
Avik Sarkar <aviksarkar0204@gmail.com>
2026-05-15 19:57:23 +0530

    Add Gaussian Naive Bayes implementation in C and update CMakeLists.txt

---
+++ Gaussian_Naive_Bayes/Gaussian_Naive_Bayes.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#define N 10
+#define F 2
+#define C 2
+
+int main()
+{
+    double x[N][F] = {
+        {150, 48},
+        {155, 52},
+        {160, 55},
+        {165, 60},
+        {170, 65},
+        {175, 70},
+        {180, 75},
+        {185, 82},
+        {190, 88},
+        {195, 95}
+    };
+    int y[10] = {0, 0, 0, 0, 0, 1, 1, 1, 1, 1};
+
+    return 0;
+}
