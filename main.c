465dd282c5c77a5863b3a275becaf837db315789
Avik Sarkar <aviksarkar0204@gmail.com>
2026-05-21 19:52:57 +0530

    Remove unused C implementation of sigmoid function and associated files

--- main.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "math_utils.h"
-
-int main() {
-    printf("%f\n", sigmoid(0.5));
-}
--- math_utils.h
+++
@@ -1,6 +0,0 @@
-#ifndef MATH_UTILS_H
-#define MATH_UTILS_H
-
-double sigmoid(double z);
-
-#endif
