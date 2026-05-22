0dde1aa4bfd7443a6dd32f3e6d647234bf2bccdd
Avik Sarkar <aviksarkar0204@gmail.com>
2026-05-22 19:34:01 +0530

    Add initial implementation of K-Means clustering in C and update CMakeLists.txt

---
+++ K-Mean/K-Mean.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+#define N 6
+#define K 2
+#define F 2
+#define MAX_ITER 100
+
+int main()
+{
+    double X[N][F] = {{1,1},
+        {1,2},
+        {2,1},
+        {8,8},
+        {8,9},
+        {9,8}};
+    double centroids[K][F];
+    int labels[N];
+}
